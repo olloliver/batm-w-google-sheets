8ef76089f4f3ce19733437ce45b18979c932d3ed
olloliver <lily-oliver@uiowa.edu>
2026-06-25 14:01:05 -0500

    Update google-doc-url.js
    new api key

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1WaHrVpK9Y8yWYpcbnMqtj7trRolgqeuvfrPvSI9CjSU/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyD5WmeSayMKDXz5nClo4m4R5sy44aZzxcE';
